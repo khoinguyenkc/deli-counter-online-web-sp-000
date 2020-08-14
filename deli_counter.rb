0e17e3cba95d8cccd09a31324ce869698c7cce05
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 07:47:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(queue)
   #parameter = an array
   if queue == []
-    return "The line is currently empty."
+    puts  "The line is currently empty."
 
   else
     queue.each_with_index do |item, index|
@@ -12,7 +12,7 @@ def line(queue)
       #must assign bvalue to queeue[index]. if u do item =... it won't do anything
     end
     list = queue.join(" ")
-    return "The line is currently: #{list}"
+    puts "The line is currently: #{list}"
   end
 
 end
