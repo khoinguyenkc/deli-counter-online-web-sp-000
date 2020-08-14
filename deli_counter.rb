2d5106d76c4cc37e77192435efe26f1977cb4faf
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 07:45:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(queue)
+  #parameter = an array
+  if queue == []
+    return "The line is currently empty.".
+
+  else
+    queue.each_with_index do |item, index|
+      queue[index] = "#{index+1}. #{item}"
+      #must assign bvalue to queeue[index]. if u do item =... it won't do anything
+    end
+    list = queue.join(" ")
+    return "The line is currently: #{list}"
+  end
+end
+
+puts line(["Logan", "Avi", "Spencer"])
